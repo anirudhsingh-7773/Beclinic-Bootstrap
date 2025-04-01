d0085cd83a782c31bcdd285eca3d2435bce96ca4
Anirudh Singh <anirudh.singh.7773@gmail.com>
2025-04-01 15:21:07 +0530

    script file corrections

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,18 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Select primary header
   const header_primary = document.querySelector(".header__primary");
-  const search_icon = document.querySelector(".header__primary__dropdown");
-  const header = document.querySelector(".header");
 
-    // Handles primary menu display
-    search_icon.addEventListener("click", function () {
-      if (window.getComputedStyle(header_primary).display === "none") {
-        header_primary.classList.add("active");
-        header_primary.classList.remove("inactive");
-      }
-      else {
-        header_primary.classList.remove("active");
-        header_primary.classList.add("inactive");
-      }
-    });
+  // Select search icon
+  const search_icon = document.querySelector(".header__primary__dropdown");
+
+  // Handles primary menu display
+  search_icon.addEventListener("click", function () {
+    if (window.getComputedStyle(header_primary).display === "none") {
+      header_primary.classList.add("active");
+      header_primary.classList.remove("inactive");
+    }
+    else {
+      header_primary.classList.remove("active");
+      header_primary.classList.add("inactive");
+    }
+  });
 });
